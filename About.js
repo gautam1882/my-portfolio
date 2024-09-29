b47f4005ff77f8ef1a62d527a9be9b0baf20f8c3
gautam1882 <gautamarora56167@gmail.com>
2024-09-28 23:25:09 -0400

    Add files via upload

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Hello! I'm Gautam Arora, a web developer with a passion for creating amazing websites and applications.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+            <p>You can reach me via email at gautamaroar567@gmail.com or follow me on social media.</p>
+        </div>
+    );
+}
+
+export default Contact;
